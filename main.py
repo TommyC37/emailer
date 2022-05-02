fa89e3d859b859568ba7fbbcd0b79e8a12974713
Tom Cobb <tom@tomcobbcodes.com>
2022-05-01 22:06:11 -0400

    Added scheduled send

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import smtplib
+import datetime as dt
 from decouple import config
+import time
 
 with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+  connection.ehlo()
   connection.starttls()
 
   from_addr = config('FROM_ADDR')
   from_password = config('FROM_PASS')
 
   to_addr = config('TO_ADDR')
-  subject = 'I just sent this email from Python!'
-  body = f"Subject:{subject}\n\n\HeyTom,\n\nHow crazy is this?\n\n\I wonder if I can use this strange new power to build an email automation software for salespeople (that doesn't suck).\n\n\-Tom"
+  to_name = input('To name: ')
+  subject = input('Subject: ')
+  message = input('Message: ')
+  from_name = input('From name: ')
+  send_time = dt.datetime(2022,5,1,21,25,0,0)
+  body = f"Subject:{subject}\n\nHi {to_name},\n\n{message}\n\nCheers,\n{from_name}"
+
+  x=time.sleep(send_time.timestamp() - time.time())
 
   connection.login(user=from_addr, password=from_password)
   connection.sendmail(from_addr=from_addr, to_addrs=to_addr, msg=body)
